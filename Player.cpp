7c9ed36e6269c365f0d6a4e26e3207c645c2a817
Harbulot Xavier <xavier.harbulot@hotmail.fr>
2017-11-26 13:53:40 +0100

    Added multiple key support

--- Player.cpp
+++ Player.cpp
@@ -36,26 +36,68 @@ void Player::fire(){
 
 void Player::keyPressEvent(QKeyEvent *event){
 
+    m_bFirstrealase = true;
+    keysPressed.insert(event->key());
+
+    /*
     //Mouvements du joueur.
     if(event->key() == Qt::Key_Left) {
+        qDebug() << "loul left";
         if(pos().x() > 0)
             setPos(x()- playerStats.playerSpeed,y());
     }
-    else if (event->key() == Qt::Key_Right) {
+    if (event->key() == Qt::Key_Right) {
         if(pos().x() + pixmap().width() < width_scene)
             setPos(x()+ playerStats.playerSpeed,y());
     }
-    else if (event->key() == Qt::Key_Up) {
+    if (event->key() == Qt::Key_Up) {
+        qDebug() << "loul haut";
         if(pos().y() > 0)
             setPos(x(),y()- playerStats.playerSpeed);
     }
-    else if (event->key() == Qt::Key_Down) {
+    if (event->key() == Qt::Key_Down) {
+        qDebug() << "loul bas";
         if(pos().y() + pixmap().height() < height_scene)
             setPos(x(),y()+ playerStats.playerSpeed);
     }
 
     //Projectile.
     if(event->key() == Qt::Key_Space) {
+        fire();
+    }
+    */
+}
+
+void Player::keyReleaseEvent(QKeyEvent *event){
+    if(m_bFirstrealase){
+        KeysProcessing(keysPressed);
+    }
+
+    m_bFirstrealase = false;
+    keysPressed.remove(event->key());
+}
+
+void Player::KeysProcessing(QSet<int> keys){
+
+    if(keys.contains(Qt::Key_Left)) {
+        if(pos().x() > 0)
+            setPos(x()- playerStats.playerSpeed,y());
+    }
+    if (keys.contains(Qt::Key_Right)) {
+        if(pos().x() + pixmap().width() < width_scene)
+            setPos(x()+ playerStats.playerSpeed,y());
+    }
+    if (keys.contains(Qt::Key_Up)) {
+        if(pos().y() > 0)
+            setPos(x(),y()- playerStats.playerSpeed);
+    }
+    if (keys.contains(Qt::Key_Down)) {
+        if(pos().y() + pixmap().height() < height_scene)
+            setPos(x(),y()+ playerStats.playerSpeed);
+    }
+
+    //Projectile.
+    if(keys.contains(Qt::Key_Space)) {
         fire();
     }
 }
--- Player.h
+++ Player.h
@@ -15,8 +15,13 @@ public:
     Player(QGraphicsItem *parent = 0);
     void fire();
     void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void KeysProcessing(QSet<int> keys);
+
 private:
     QMediaPlayer * bulletSound;
+    QSet<int> keysPressed;
+    bool m_bFirstrealase;
 
     PlayerStats playerStats;
 public slots:
